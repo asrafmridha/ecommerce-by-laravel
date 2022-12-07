eb611542a41a7e4431f6917f87bfef06cf3bacd7
asraf-mridha <asrafmridha@gmail.com>
2022-12-08 01:22:49 +0600

    Create a blade file for childcategory edit and show value

---
+++ resources/views/backend/admin/category/edit_childcategory.blade.php
@@ -0,0 +1,43 @@
+@extends('backend.mastaring.master')
+@section('category','active')
+
+@section('content')
+ 
+
+        <form action="{{ route('childcategory.update',$childcategory->id) }}" method="POST">
+            @method('PUT')
+                @csrf
+                    <label for="subcategory_name">Category/SubCategory Name</label>
+                    <select class="form-control" aria-label="Default select example" name="subcategory_id">
+
+                        @foreach ($category as $category)
+                            @php
+                                
+                                $subcategory = DB::table('sub_categories')->where('category_id', $category->id)->get();
+                            
+                            @endphp 
+                                <option>{{ $category->category_name }}</option>
+                        
+                       
+                            @foreach ($subcategory as $subcat)
+                            <option value="{{ $subcat->id }}">-----  {{ $subcat->sub_category_name }}  ------</option>
+
+                            @endforeach
+                        @endforeach
+
+                    </select>    
+                    <label for="sub_category_name">Enter ChildCategory Name</label>
+                    <input type="text" name="child_category_name" class="form-control import" value="{{ $childcategory->childcategory_name }}">
+                    @error('sub_category_name')
+                    <div class="alert alert-danger">
+                        {{$message}}
+                    </div>  
+                    @enderror 
+                
+            </div>
+
+                        <div class="col-12 mt-1">
+                            <button type="submit" class="btn btn-primary mr-1">{{ __('Update') }}</button>
+                        </div>
+        </form>
+                @endsection
