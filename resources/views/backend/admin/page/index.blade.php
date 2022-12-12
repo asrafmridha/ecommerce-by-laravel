1f29f71ddc251f3f72581450ffa99de94e838e94
asraf-mridha <asrafmridha@gmail.com>
2022-12-13 04:09:53 +0600

    Design for Page update and show value

--- resources/views/backend/admin/page/index.blade.php
+++ resources/views/backend/admin/page/index.blade.php
@@ -1,5 +1,5 @@
 @extends('backend.mastaring.master')
-@section('category','active')
+@section('setting','active')
 @section('breadcrumb')
     <h2 class="content-header-title float-left mb-0">Admin Dashboard</h2>
     <div class="breadcrumb-wrapper">
@@ -18,7 +18,7 @@
 {{-- Data Filter Start --}}
 <div class="card-body">
     <div class="btn-group"> 
-        <a href="{{ route('page.create') }}"  class="end btn btn-primary">Add Page</a>  
+        <a href="{{ route('page.create') }}"  class="end btn btn-primary">Add Page</a> 
         <a data-toggle="modal" data-target="#feedbackcsvModal" class="end btn btn-success"
             href="">Import</a>
             
@@ -101,10 +101,10 @@
                             <th>Page Name</th>
                             <th>Page Title</th>
                             <th>Action</th>
-                            </tr>
-                            </thead>
+                        </tr>
+                    </thead>
                             <tbody>
-                                  @foreach ( $page as $page) 
+                            @foreach ( $page as $page) 
                                 <tr>
                                     <td>
                                         <div class="custom-control custom-control-primary custom-checkbox">
@@ -173,20 +173,77 @@
                                                     </button>
                                                 </div>
                                                 <div class="modal-body">
-                                                    <form action="{{ route('category.update',$page->id) }}" method="POST">
-                                                        @method('PUT')
+                                                    <form action="{{ route('page.update',$page->id) }}" method="POST">
+                                                        
                                                     @csrf
-                                                    <label for="category_name">Enter  Update Category Name</label>
-                                                    <input type="text" name="category_name" class="mt-1 form-control import" value="{{ $page->category_name }}" >
-                                                        @error('category_name')
+
+                                                <div class="col-12">
+                                                    <div class="form-group">
+                                                        <label for="page_position">Page Position<small class="text-danger">*</small></label>
+                                                        <select name="page_position" id="" class="form-control">
+                                                            <option value="1" @if($page->page_position==1) selected  @endif>Line One</option>
+                                                            <option value="1" @if($page->page_position==2) selected  @endif>Line Two</option>
+                                                            
+                                                        </select>
+
+                                                        </div>
+                                                        @error('page_position')
+                                                            <div class="alert alert-danger">
+                                                                <div class="alert-body">
+                                                                    {{ $message }}
+                                                                </div>
+                                                            </div>
+                                                        @enderror
+                                                </div>
+
+                                                <div class="col-12">
+                                                    <div class="form-group">
+                                                        <label for="page_name">Page Name<small class="text-danger">*</small></label>
+                                                        <input type="text" class="form-control" id="title" value="{{ $page->page_name }}" name="page_name" placeholder="Enter Coupon Name">
+                                                    </div>
+                                                    @error('page_name')
                                                         <div class="alert alert-danger">
-                                                            {{$message}}
-                                                        </div>  
-                                                        @enderror 
+                                                            <div class="alert-body">
+                                                                {{ $message }}
+                                                            </div>
+                                                        </div>
+                                                    @enderror
+                                                </div>
+
+                                                <div class="col-12">
+                                                    <div class="form-group">
+                                                        <label for="page_title">Page Title<small class="text-danger">*</small></label>
+                                                        <input type="text" class="form-control" id="title" name="page_title" placeholder="Enter Page Title" value="{{ $page->page_title }}">
+                                                    </div>
+                                                    @error('page_title')
+                                                        <div class="alert alert-danger">
+                                                            <div class="alert-body">
+                                                                {{ $message }}
+                                                            </div>
+                                                        </div>
+                                                    @enderror
+                                                </div>
+
+                                                <div class="col-12">
+                                                    <div class="form-group">
+                                                        <label for="short_description">{{'Enter Page Description'}} <small class="text-danger">*</small></label>
+                                                        <div>                                    
+                                                            <textarea name="page_description" placeholder="Underground Short Description" class="form-control">{{ $page->page_description }}</textarea>
+                                                        </div>
+                                                    </div>
+                                                    @error('page_descriptiontion.en')
+                                                        <div class="alert alert-danger">
+                                                            <div class="alert-body">
+                                                                {{ $message }}
+                                                            </div>
+                                                        </div>
+                                                    @enderror
+                                                </div>
+                                                    
                                                 </div>
                                                 <div class="modal-footer">
                                                     <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                                                    <button type="submit" class="btn btn-primary">Save</button>
+                                                    <button type="submit" class="btn btn-primary">Update</button>
                                                 </div>
                                                     </form>
                                             </div>
@@ -242,16 +299,7 @@
             <div class="modal-body">
                 <form action="{{ route('category.store') }}" method="POST">
                 @csrf
-                <label for="category_name">Page Position </label>
-                <select name="" class="form-control" >
-                    <option value="1">Line One</option>
-                    <option value="2">Line Two</option>
-                </select>
-                    @error('category_name')
-                    <div class="alert alert-danger">
-                        {{$message}}
-                    </div>  
-                    @enderror 
+              
 
                 <label for="category_name" class="mt-1"></label>Page Name</label>
                 <input type="text" name="category_name" class=" form-control import" >
