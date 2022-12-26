e2b0806174a2574f8f82bd517b40bd47d4cc3edb
asraf-mridha <asrafmridha@gmail.com>
2022-12-26 21:10:34 +0600

    Cupon Crud done

---
+++ app/Http/Controllers/Backend/CuponController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Http\Controllers\Backend;
+
+use App\Http\Controllers\Controller;
+use App\Models\Cupon;
+use Illuminate\Http\Request;
+
+class CuponController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $cupon = Cupon::all();
+        return view('backend.admin.cupon.index', compact('cupon'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        Cupon::create($request->except('_token'));
+        return back()->withSuccess('Cupon Added Successfully');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        Cupon::find($id)->update($request->except('_token'));
+        return back()->withSuccess('Cupon Updated Successfully');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        Cupon::find($id)->delete();
+        return back()->withSuccess('Cupon Deleted Successfully');
+    }
+}
