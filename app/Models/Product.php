4fcf3080665ef05e5f60d93147a72a6a13e776e2
asraf-mridha <asrafmridha@gmail.com>
2023-01-01 22:54:20 +0600

    Join belongsTo Product Model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,4 +8,28 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+    public function category()
+    {
+        return $this->belongsTo(Category::class, 'category_id');
+    }
+
+    public function subcategory()
+    {
+        return $this->belongsTo(SubCategory::class, 'subcategory_id');
+    }
+
+    public function childcategory()
+    {
+        return $this->belongsTo(ChildCategory::class, 'childcategory_id');
+    }
+
+    public function brand()
+    {
+        return $this->belongsTo(Brand::class, 'brand_id');
+    }
+
+    public function pickuppoint()
+    {
+        return $this->belongsTo(Pickuppoint::class, 'pickup_point_id');
+    }
 }
