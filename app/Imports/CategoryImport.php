c06e3970bebcf0f1ac374d9ba7be962a06439138
asraf-mridha <asrafmridha@gmail.com>
2023-01-24 10:53:02 +0600

    Category Slug generate When File Import

--- app/Imports/CategoryImport.php
+++ app/Imports/CategoryImport.php
@@ -5,6 +5,7 @@ namespace App\Imports;
 use App\Models\Category;
 use Maatwebsite\Excel\Concerns\ToModel;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
+use Illuminate\Support\Str;
 
 class CategoryImport implements ToModel, WithHeadingRow
 {
@@ -17,6 +18,7 @@ class CategoryImport implements ToModel, WithHeadingRow
     {
         return new Category([
             'category_name' => $row['category_name'],
+            'category_slug' => Str::slug($row['category_name']),
         ]);
     }
 }
