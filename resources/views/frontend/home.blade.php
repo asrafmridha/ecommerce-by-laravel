c2d6524378f0d24cf57f6f85b5d844040e44759f
asraf-mridha <asrafmridha@gmail.com>
2023-01-29 20:56:06 +0600

    Banner Product Show Frontend Successfully

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -23,22 +23,26 @@
 	
 	<!-- Banner -->
 
+   @if(($banner_product==null))
+        <h1 class="m-5">No Product Found</h1>
+	@else	
 	<div class="banner">
-		<div class="banner_background" style="background-image:url(images/banner_background.jpg)"></div>
+		<div class="banner_background" style="background-image:url({{ asset('uploads/product/'.$banner_product->thumbnails) }})" ></div>
 		<div class="container fill_height">
 			<div class="row fill_height">
 				<div class="banner_product_image"><img src="images/banner_product.png" alt=""></div>
 				<div class="col-lg-5 offset-lg-4 fill_height">
 					<div class="banner_content">
-						<h1 class="banner_text">new era of smartphones</h1>
-						<div class="banner_price"><span>$530</span>$460</div>
-						<div class="banner_product_name">Apple Iphone 6s</div>
+						<h1 class="banner_text">{{ $banner_product->name }}</h1>
+						<div class="banner_price"><span>$530</span>{{ $banner_product->selling_price }}</div>
+						<div class="banner_product_name">{{ $banner_product->brand->brand_name }}</div>
 						<div class="button banner_button"><a href="#">Shop Now</a></div>
 					</div>
 				</div>
 			</div>
 		</div>
 	</div>
+@endif	
 
 	<!-- Characteristics -->
 
