5396593e9e3cd54fb3a82055b867c6c3965c9c6c
asraf-mridha <asrafmridha@gmail.com>
2023-02-03 23:32:21 +0600

    Wishlist Added Successfully

---
+++ app/Http/Controllers/WishlistController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Wishlist;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+
+class WishlistController extends Controller
+{
+    public function wishlist($id)
+    {
+
+        $check = DB::table('wishlists')->where('product_id', $id)->where('user_id', Auth::id())->first();
+        if ($check) {
+            return back()->with('error', 'Product Alreay in Wishlist');
+        } else {
+            $wishlist = new Wishlist();
+            $wishlist->user_id      = Auth::user()->id;
+            $wishlist->product_id   = $id;
+            $wishlist->save();
+            return back()->with('message', 'Product Add in Wishlist');
+        }
+    }
+}
