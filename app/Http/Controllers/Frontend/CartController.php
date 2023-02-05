abccce2f7c4ab18dbe7fd726764ac71a9d9197e1
asraf-mridha <asrafmridha@gmail.com>
2023-02-05 20:54:52 +0600

    Cart Details Page

--- app/Http/Controllers/Frontend/CartController.php
+++ app/Http/Controllers/Frontend/CartController.php
@@ -3,9 +3,11 @@
 namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
+use App\Models\Category;
 use App\Models\Product;
 
 use Cart;
+use GrahamCampbell\ResultType\Success;
 use Illuminate\Http\Request;
 
 class CartController extends Controller
@@ -20,10 +22,27 @@ class CartController extends Controller
             'qty'   => 1,
             'price' => $product->selling_price,
             'weight' => 1,
+            'options' => ['thumnails' => $product->thumbnails],
 
         ]);
-        return response()->json([
-            'success' => "Add to Cart Successfully",
-        ]);
+        return back()->withSuccess("Added Cart Successfully");
+    }
+
+    public function allcart()
+    {
+        $data = array();
+        $data['cart_qty']   = Cart::count();
+        $data['cart_total'] = Cart::total();
+        return response()->json($data);
+    }
+
+    public function cartDetails()
+    {
+        $banner_product = Product::where('status', 'on')->latest()->first();
+        $categorires = Category::all();
+        $featured = Product::where('featured', 'on')->where('status', 'on')->orderBy('id', 'DESC')->limit(8)->get();
+        $cartDetails = Cart::content();
+        // return response()->json($cartDetails);
+        return view('frontend.cart.cart_details', compact('categorires', 'cartDetails'));
     }
 }
