a9ade4d69a5f8b95c4cb0ab31c534afc2961acb0
asraf-mridha <asrafmridha@gmail.com>
2023-02-06 22:40:32 +0600

    Checkout page Design

--- resources/views/frontend/checkout/index.blade.php
+++ resources/views/frontend/checkout/index.blade.php
@@ -20,78 +20,96 @@
 	<!-- Cart -->
 
 	<div class="cart_section">
+        
 		<div class="container">
-                <div class="row">
-                    <div class="col-lg-10 offset-lg-1">
-                        <div class="cart_container">
-                            <div class="cart_title">Checkout</div>
-                            <div class="cart_items">
-                            
-                                <ul class="cart_list">
-                                @foreach($cartDetails as $details)
-                                    <li class="cart_item clearfix">
-                                        <div class="cart_item_image"><img src="{{ asset('uploads/product/'.$details->options->thumbnails) }}" alt=""></div>
-                                        <div class="cart_item_info d-flex flex-md-row flex-column justify-content-between">
-                                            <div class="cart_item_name cart_info_col">
-                                                <div class="cart_item_title">Name</div>
-                                                <div class="cart_item_text">{{ $details->name }}</div>
-                                            </div>
-                                            <div class="cart_item_color cart_info_col">
-                                                <div class="cart_item_title">Color</div>
-                                                <div class="cart_item_text"><span style="background-color:#999999;"></span>Silver</div>
-                                            </div>
-                                            <div class="cart_item_quantity cart_info_col">
-                                                <div class="cart_item_title">Quantity</div>
-                                                <div class="cart_item_text">1</div>
-                                            </div>
-                                            <div class="cart_item_price cart_info_col">
-                                                <div class="cart_item_title">Price</div>
-                                                <div class="cart_item_text">{{ $details->qty }}</div>
-                                            </div>
-                                            <div class="cart_item_total cart_info_col">
-                                                <div class="cart_item_title">Total</div>
-                                                <div class="cart_item_text">$2000</div>
-                                            </div>
-                                              <div class="cart_item_total cart_info_col">
-                                                <div class="cart_item_title">Action</div>
-                                                <div class="cart_item_text"><a href="{{ route('cart.remove',$details->rowId) }}">X</a></div>
-                                            </div>
-                                        </div>
-                                    </li>
-                                @endforeach
-                                </ul>
-                            
-                            </div>
-
-                            @php
-                                
-                            @endphp
-                            
-                            <!-- Order Total -->
-                       
-                            <div class="order_total">
-                                <div class="order_total_content text-md-right">
-                                    <div class="order_total_title">Order Total:</div>
-                                    <div class="order_total_amount">{{ Cart::total() }}</div>
+            <div class="row">
+                <div class="col-lg-8">
+                    <div class="cart_container card p-1">
+                        <div class="cart_title text-center">Billing Address</div>
+                        <form action="">
+                            <div class="row p-4">
+                                <div class="form-group col-lg-6">
+                                    <label for="">Customer Name</label>
+                                    <input type="text" class="form-control" value="{{ ucwords(Auth::user()->name) }}" name="name" placeholder="Customer Name">
                                 </div>
-                            </div>
-                            
-                           
-                        @if(Cart::total()!=0)
-                            <div class="container">
-                                <div class="row">
-                                    <div class="col-md-12 bg-light text-right">
-                                        <a href="{{ route('cart.remove.all') }}" class="btn btn-primary">Cart clear</a>
-                                        <a href="{{ route('checkout') }}" class="btn btn-warning">Checkout</a>
+                                <div class="form-group col-lg-6">
+                                    <label for="">Customer Phone</label>
+                                    <input type="text" class="form-control" value="{{ ucwords(Auth::user()->phone) }}" name="phone" placeholder="Customer Name">
+                                </div>
+                                <div class="form-group col-lg-6">
+                                    <label for="">Country</label>
+                                    <input type="text" class="form-control"  name="country" placeholder="Customer Country">
+                                </div>
+                                <div class="form-group col-lg-6">
+                                    <label for="">Shipping Address</label>
+                                    <input type="text" class="form-control"  name="address" placeholder="Customer Address">
+                                </div>
+                                 <div class="form-group col-lg-6">
+                                    <label for="">Email</label>
+                                    <input type="email" class="form-control"  name="email" placeholder="Customer Email">
+                                </div>
+                                 <div class="form-group col-lg-6">
+                                    <label for="">Zip Code</label>
+                                    <input type="text" class="form-control"  name="zip" placeholder="Customer Zip Code">
+                                </div>
+                                 <div class="form-group col-lg-6">
+                                    <label for="">Extra Address</label>
+                                    <input type="text" class="form-control"  name="e_address" placeholder="Customer Extra Address">
+                                </div>
+                                 <div class="form-group col-lg-6">
+                                    <label for="">Extra Phone</label>
+                                    <input type="text" class="form-control"  name="e_phone" placeholder="Customer Extra Phone">
+                                </div>
+                                <strong>Payment Method</strong>
+                                <div class="row ml-5">
+                                    <div class="form-group col-lg-4 ">
+                                        <label for="">Paypal</label>
+                                        <input type="radio" class="form-control mt-1" name="payment_type">
                                     </div>
+                                     <div class="form-group col-lg-4">
+                                        <label for="">SSL COMMERZ</label>
+                                        <input type="radio" class="form-control" name="payment_type">
+                                    </div>
+
+                                     <div class="form-group col-lg-4">
+                                        <label for="">Hand Cash</label>
+                                        <input type="radio" class="form-control" name="payment_type">
+                                    </div>
+
+
                                 </div>
                             </div>
-                        @endif
-                        </div>
+                            <div class="form-group ml-3">
+                                <button type="submit" class="btn btn-info">Order Place</button>
+                            </div>
+                        </form>
                     </div>
                 </div>
-		</div>
-	</div>
+                <div class="col-lg-4">
+                    <div class="card">
+                        <label for="">Subtotal:</label>
+                        <label for="">Coupon:</label>
+                        <label for="">Tax:</label>
+                        
+                        
+                        <form action="">
+
+                            <div class="p-4">
+                                <label for="">Coupon Apply</label>
+                                <input type="text" class="form-control" name="coupon" placeholder="Coupon Code">
+
+                                <div class="form-group mt-2">
+                                    <button type="submit" class="btn btn-info">Apply</button>
+                                </div>
+
+                            </div>
+                        </form>
+                    </div>
+                 
+                </div>
+		    </div> 
+            
+	    </div>
 
 	<!-- Newsletter -->
 
